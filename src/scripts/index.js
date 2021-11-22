2933b240ee94dd24ec0dafdbf2721f4ed1d70838
Keli Booster <kbooster17@gmail.com>
2021-11-22 19:56:05 +0000

    add remove book functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
-const books = [];
-const addButton = document.querySelector('.add-btn');
+let books = [];
+const addButton = document.querySelector(".add-btn");
+const bookList = document.querySelector(".books-ul");
 
 function Book(name, title) {
   this.name = name;
@@ -7,26 +8,37 @@ function Book(name, title) {
 }
 
 function addBook() {
-  const bookName = document.querySelector('.bookname').value;
-  const bookTitle = document.querySelector('.author').value;
+  const bookName = document.querySelector(".bookname").value;
+  const bookTitle = document.querySelector(".author").value;
 
   const book = new Book(bookName, bookTitle);
   books.push(book);
 }
 
 function showBooks() {
-  let book = '';
-  const bookList = document.querySelector('.books-ul');
-  books.forEach((bookObj) => {
-    book += `<li class="book-li">
-    <span>${bookObj.name}</span><span>${bookObj.title}</span>
-    <input type="button" class="btn" value="Remove" />
+  let book = "";
+  if (books.length == 0) {
+    bookList.innerHTML = "<p>Sorry you have no book left. Kindly add some</p>";
+  } else {
+    books.forEach((bookObj, ind) => {
+      book += `<li class="book-li">
+    <span>${bookObj.name}</span> <span>${bookObj.title}</span>
+    <input type="button" class="btn rmv" data-target=${ind} value="Remove" />
   </li>`;
-    bookList.innerHTML = book;
-  });
+      bookList.innerHTML = book;
+    });
+  }
 }
+bookList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("rmv")) {
+    books = books.filter(
+      (book, index) => index != e.target.attributes[2].value
+    );
+    showBooks();
+  }
+});
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   addBook();
   showBooks();
 });
